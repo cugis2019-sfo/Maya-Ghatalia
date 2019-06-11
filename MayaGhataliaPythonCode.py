15b7c0b26f85815a325a14b91888c2b4630f877c
mayarg <51681139+mayarg@users.noreply.github.com>
2019-06-11 10:40:29 -0700

    Add files via upload

--- MayaGhataliaPythonCode.py
+++ MayaGhataliaPythonCode.py
@@ -5,27 +5,3 @@ Spyder Editor
 This is a temporary script file.
 """
 
-5/8 #division
-print ("I am learning to code in python today")
-print ("Hey",3,"There")
-print (hello)
-("5")
-5*2 #multipication
-5/2 #division
-5-2 #subtraction
-5**2 #exponent
-(8/9)*3 #dividethenmultiply
-#writing code to sum any two number
-def sum(a,b,c):
-    sum = a + b + c
-    
-    print("the sum is", sum)
-    
-
-sum(5,8,12)
-    def areaoftri (b,h):
-            areaoftri = (b*h)/2
-            
-            print ("the area is",areaoftri,"units squared")
-            
-    areaoftri(5,6)
